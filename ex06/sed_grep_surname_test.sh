a332467f4f6f03b1cb950106392c52936ec3484a
ashanaakh <ashanaakh@gmail.com>
2017-07-27 20:13:28 +0300

    Ex 6 fix

--- ex06/sed_grep_surname_test.sh
+++ ex06/sed_grep_surname_test.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bats
 
 @test "Test work on first 5 lines" {
-  run bash script.sh 5
+  run bash sed_grep_surname.sh 5
 
   [ "$status" -eq 0 ]
   [ "$output" = $'BROWN  5429595\nWILLIAMS  4173254' ]
 }
 
 @test "No parameter returns error" {
-  run bash script.sh
+  run bash sed_grep_surname.sh
 
  [ "$status" -eq 1 ]
 }
 
 @test "Test work on first 10 lines" {
-  run bash script.sh 10
+  run bash sed_grep_surname.sh 10
 
   [ "$status" -eq 0 ]
   [ "$output" = $'BROWN  5429595\nWILLIAMS  4173254\nMILLER  3391134\nTAYLOR  3337476\nWILSON  3299960\nDAVIS  3212807\nWHITE  2881963' ]
 }
 
 @test "Test on 0 lines returns error" {
-  run bash script.sh 0
+  run bash sed_grep_surname.sh 0
 
   [ "$status" -eq 1 ]
 }
 
 @test "Test on 2 lines return empty string" {
-  run bash script.sh 2
+  run bash sed_grep_surname.sh 2
 
   [ "$status" -eq 0 ]
   [ "$output" = $'' ]
