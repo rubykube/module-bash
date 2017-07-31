46e319699c1536540ac4af325a2b1ab0a67a6467
emasiuk <toysoldier1927@gmail.com>
2017-07-31 16:23:26 +0300

    improve readme and rename test for ex03 (#24)

--- ex03/README.md
+++ ex03/README.md
@@ -2,7 +2,7 @@
 
 Write a script that takes a filename and 3 keywords. It should
 `grep` in the file for all 3 keywords and display for each keyword the number of
-matches.
+matches and the lines where they meeting.
 
 - No other output on *stdout* should be produced by the script
 - If the file cannot be read the script should exit with a return code 1, else
---
+++ ex03/test_ex03.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+@test "Default" {
+  run bash ex03.sh resources/1 Ipsum is and
+
+  [ "$status" -eq 0 ]
+  [ "$output" = "Ipsum 4
+1
+2
+7
+9
+is 3
+1
+6
+8
+and 4
+1
+2
+3
+8" ]
+}
+
+@test "No args" {
+  run bash ex03.sh
+
+  [ "$status" -eq 1 ]
+  [ "$output" = '' ]
+}
+
+@test "Without right args" {
+  run bash ex03.sh resources/1 Helios Technologies Great
+
+  [ "$status" -eq 0 ]
+  [ "$output" = 'Helios 0
+Technologies 0
+Great 0' ]
+}
+
+
+@test "With empty lines" {
+  run bash ex03.sh resources/3 like page sometimes
+
+  [ "$status" -eq 0 ]
+  [ "$output" = 'like 2
+7
+36
+page 2
+2
+10
+sometimes 2
+13
+36' ]
+}
+
+@test "with number" {
+  run bash ex03.sh resources/4 1.10.32 2000 45
+
+  [ "$status" -eq 0 ]
+  [ "$output" = "1.10.32 3
+7
+12
+15
+2000 1
+3
+45 2
+2
+9" ]
+}
