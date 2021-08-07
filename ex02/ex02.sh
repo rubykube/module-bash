34838c57f6ecf531b8e1205b5f8b2d64459cdd52
Bohdan Panamarenko <panamarenkobohdan@gmail.com>
2021-08-07 12:07:26 +0000

    Syntax fixed

--- ex02/ex02.sh
+++ ex02/ex02.sh
@@ -1,17 +1,16 @@
 #!/etc/bash
 
-if [ -z "$1" ]; then
-    echo "Wrong number of arguments: expected 1, given 0"
+if [[ -z "${1}" ]]; then
+    echo "Wrong number of arguments: expected 1, given 0" 1>&2
     exit 1
-else
-    for element in $1
-    do
-        RESPONSE=`ls -R ./ | grep $element`
-        if [ -z "$RESPONSE" ]; then
-            echo "the searched PATH is unexisting"
-        else
-            echo "$RESPONSE"
-        fi
-
-    done
 fi
+
+for element in ${1}; do
+    paths=`ls -R ./ | grep ${element}`
+    if [[ -z "${paths}" ]]; then
+        echo "the searched PATH is unexisting"
+    else
+        echo "${paths}"
+    fi
+done
+
