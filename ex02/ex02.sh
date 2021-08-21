c49df3a5048012797b51b308338786b43f139dea
MykytaOksak <stupidmaadworld@gmail.com>
2021-08-21 08:15:24 -0700

    ex02 solution

---
+++ ex02/ex02.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+############################################
+# Recursive file search
+# Arguments:
+#   ONLY FULL WORDS (e.g. FIND, FOLDER, etc)
+# Outputs:
+#   all directories below the current 
+#   working directory, which have a relative
+#   path, that matches the pattern
+############################################
+rec_search() {
+    for file in $1
+    do
+        path=$(ls -R ./ | grep ${file})
+        if [ -z "${path}" ]; then
+            echo "the searched PATH is unexisting"
+        else
+            echo "${path}"
+        fi
+
+    done
+}
+
+rec_search $1
