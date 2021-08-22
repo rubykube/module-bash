f770bf5436dbdcd2c0e1354a537500156bbab5ff
Kateryna Mozghova <mozgoprod@gmail.com>
2021-08-22 08:14:44 +0300

    ex01: added the hello_world script

---
+++ ex01/hello_world.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+hello_world() {
+     if [[ -n "${1}" ]]; then
+         echo "Hello, ${1}!"
+     else
+         echo "Hello, World!"    
+     fi        
+ }
+
+    hello_world $1 
