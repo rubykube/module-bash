7d54c7a3494791da4f810cb7e944fed2791f583f
junkjuk <junkjuk5645@gmail.com>
2021-10-31 13:14:52 +0200

    completed ex02

---
+++ ex02/ex02.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+for folder in $1;
+do
+  path=$(ls -R ./ | grep $folder)
+  if [ -z "$path" ]; then
+    echo "the searched PATH is unexisting"
+  else
+    echo "$path"
+  fi
+done
+
